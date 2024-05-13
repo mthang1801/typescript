324bb7ff245b0ebbbd3cc857b51d24c9236104c6
mthang1801 <mthang1801@gmail.com>
2024-05-14 05:23:28 +0700

    update new ts

--- src/data-structures/linked-list/type.ts
+++
@@ -1,7 +0,0 @@
-import LinkedListNode from "./node";
-
-export type List<T> = {
-	head: LinkedListNode<T>;
-	tail: LinkedListNode<T>;
-	size: number;
-};
--- src/utils/utils.ts
+++
@@ -1,4 +0,0 @@
-export const typeOf = (
-	value: any
-): 'string' | 'number' | 'array' | 'object' | 'symbol' | 'bigint' | 'undefined' | 'null' | 'boolean' =>
-	Object.prototype.toString.call(value).slice(8, -1).toLowerCase();
